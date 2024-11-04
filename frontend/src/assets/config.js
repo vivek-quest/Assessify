5c8ce2185657380335319fdd0bf4be902650a63a
Paras <paras.k@questlabs.biz>
2024-11-04 15:32:43 +0530

    BE link updated

--- frontend/src/assets/config.js
+++ frontend/src/assets/config.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = 'http://localhost:5000/api';
+export const BACKEND_URL = 'https://assessify-xrvs.onrender.com/api';
 export const X_API_KEY = 'J7IgqqNwt9V7J9Ul4ZEjXdyCzX0ojC';
